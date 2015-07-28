fd3743dcc9c4025acad1fe2fb426d02de74e43f5
aramcpp <aramcpp@gmail.com>
2015-07-28 10:50:43 +0000

    main services changes complete, need to test

--- routes/services.js
+++ routes/services.js
@@ -86,48 +86,7 @@ service.post('/IWantToPlay', function(req, res, next) {
  * @params:   JSON formatted userid and areaid
  * @response: returns the cat states for all the cats in current area
  */
-// TODO: change code to use model
 service.post('/IWantToVisit', function(req, res, next) {
-  /*// connect to db
-  mysql.getConnection(function(err, connection){
-    if (err) {
-      connection.release();
-      console.log('error');
-      res.json({"status" : "Error: cant connect to db"});
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-    
-    // make a request
-    connection.query('update actions set areaid = "' + req.body.areaid + '" where userid = "' + req.body.userid + '"',function(err,result){
-      if (!err) {
-        res.json({ "YouCanVisit": "1" });
-        // make a request for WhatIsGoingOn
-        connection.query('select * from action where areaid = "' + req.body.areaid + '"',function(err,result){
-          if (!err) {
-            var resJson = result;
-            
-            resJson.YouCanVisit = "1";
-            
-            res.json(resJson);
-          }
-          else {
-            res.json({ "status": "error making request" });
-          }
-        });
-      }
-      else {
-        res.json({ "YouCanVisit": "0" });
-      }
-    });
-
-    connection.on('error', function(err) {      
-      res.json({"status" : err.stack});
-      return;     
-    });
-  });*/
-  
   // creating a query JSON
   var queryJson = req.body;
   
